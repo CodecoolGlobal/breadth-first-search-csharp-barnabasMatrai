6d9d3ac47080a3431123c95648046c5f393fdff1
Ba Matrai <matrai.barnabas1@gmail.com>
2019-12-06 18:44:57 +0100

    add empty lines to improve code readability

--- BFS-c-sharp/Program.cs
+++ BFS-c-sharp/Program.cs
@@ -15,6 +15,7 @@ namespace BFS_c_sharp
             UserNode endNode = users[1];
 
             GraphOperation graphOperation = new GraphOperation();
+
             foreach (KeyValuePair<HashSet<UserNode>, int> friendDistance in graphOperation.GetNodeDistancesFromStartNode(startNode))
             {
                 foreach (UserNode user in friendDistance.Key)
@@ -23,7 +24,9 @@ namespace BFS_c_sharp
                 }
                 Console.WriteLine(friendDistance.Value);
             }
+
             Console.WriteLine(graphOperation.MinimumDistance(users[0], users[1]));
+
             foreach (UserNode userNode in graphOperation.FriendsOfFriendsAtDistance(users[0], 1))
             {
                 Console.WriteLine($"{userNode.FirstName} {userNode.LastName}");
